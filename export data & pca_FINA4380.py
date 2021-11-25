e536b90daa17653964f55a3a2f519e3d2c7f2ad2
Thisiss0710 <82769462+Thisiss0710@users.noreply.github.com>
2021-11-25 22:58:46 +0800

    Update export data & pca_FINA4380.py

--- export data & pca_FINA4380.py
+++ export data & pca_FINA4380.py
@@ -4,24 +4,27 @@ import datetime
 import numpy as np
 from sklearn.decomposition import PCA
 
-# import data
-start = datetime.date(2013,1,1)
-end = datetime.date.today()
-modified_start = start + datetime.timedelta(days=1)
-modified_end = end + datetime.timedelta(days=1)
-
-symbols = pd.read_csv('S&P500_ticker1.csv', usecols=['Symbol'])
-for symbol in symbols.values:
-    try:
-        stock = yf.download(symbol[0],start = modified_start, end = modified_end)
-        stock.to_csv(f'stock_data/{symbol[0]}.csv')
-    except Exception:
-        continue
+def data_download(date=datetime.date(2013,1,1)):
+    # import data
+    start = date
+    end = datetime.date.today()
+    modified_start = start + datetime.timedelta(days=1)
+    modified_end = end + datetime.timedelta(days=1)
+    
+    symbols = pd.read_csv('S&P500_ticker1.csv', usecols=['Symbol'])
+    for symbol in symbols.values:
+        try:
+            stock = yf.download(symbol[0],start = modified_start, end = modified_end)
+            stock.to_csv(f'stock_data/{symbol[0]}.csv')
+        except Exception:
+            continue
         
 # close the adj_close prices into a single matrix 'price_matrix'
+def data_preprocess(date):
 price_matrix = pd.DataFrame()
 path = 'stock_data/'
 i = 0
+symbols = pd.read_csv('S&P500_ticker1.csv', usecols=['Symbol'])
 for symbol in symbols.values:
     file_path = path + symbol[0] + '.csv'
     adj_close=pd.read_csv(file_path,usecols=['Date','Adj Close'])
