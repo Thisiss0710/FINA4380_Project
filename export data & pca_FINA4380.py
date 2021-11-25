d42f6fb681599bb79c29e55749f0e9e7c1bd3f03
Thisiss0710 <82769462+Thisiss0710@users.noreply.github.com>
2021-11-25 18:59:32 +0800

    1

--- export data & pca_FINA4380.py
+++ export data & pca_FINA4380.py
@@ -5,23 +5,23 @@ import numpy as np
 from sklearn.decomposition import PCA
 
 # import data
-start = datetime.date(2019,1,1)
+start = datetime.date(2013,1,1)
 end = datetime.date.today()
 modified_start = start + datetime.timedelta(days=1)
 modified_end = end + datetime.timedelta(days=1)
 
-symbols = pd.read_csv('D:/CUHK/yr4 sem1/FINA380/S&P500_ticker1.csv', usecols=['Symbol'])
+symbols = pd.read_csv('S&P500_ticker1.csv', usecols=['Symbol'])
 for symbol in symbols.values:
     try:
         stock = yf.download(symbol[0],start = modified_start, end = modified_end)
-        stock.to_csv(f'D:/CUHK/yr4 sem1/FINA380/project_data/{symbol[0]}.csv')
+        stock.to_csv(f'stock_data/{symbol[0]}.csv')
     except Exception:
-        print('Download failed. Check if there is any error.')
+        continue
         
 # close the adj_close prices into a single matrix 'price_matrix'
 price_matrix = pd.DataFrame()
-path = 'D:/CUHK/yr4 sem1/FINA380/project_data/'
-i=0
+path = 'stock_data/'
+i = 0
 for symbol in symbols.values:
     file_path = path + symbol[0] + '.csv'
     adj_close=pd.read_csv(file_path,usecols=['Date','Adj Close'])
@@ -35,14 +35,14 @@ for symbol in symbols.values:
     i+=1
 price_matrix.set_index(price_matrix.columns[0],inplace=True)
 price_matrix.index = pd.to_datetime(price_matrix.index,format="%Y/%m/%d")
-price_matrix.to_csv(f'D:/CUHK/yr4 sem1/FINA380/project_data/collected_adj_close.csv')
+price_matrix.to_csv('collected_adj_close.csv')
 
-# #process data and fill the blanks
+# process data and fill the blanks
 grouped_price_matrix = price_matrix.groupby(pd.Grouper(freq='W')).tail(1)
 grouped_price_matrix.index = pd.to_datetime(grouped_price_matrix.index,format="%Y/%m/%d")
 grouped_price_matrix.interpolate(method='cubicspline',axis='columns',inplace=True)
 grouped_price_matrix.dropna(axis='columns',inplace=True)
-price_matrix.to_csv(f'D:/CUHK/yr4 sem1/FINA380/project_data/collected_adj_close1.csv')
+price_matrix.to_csv('collected_adj_close1.csv')
 
 # PCA
 factors=pd.DataFrame()
