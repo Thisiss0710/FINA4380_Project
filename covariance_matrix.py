01e0999908ff78e1ffff270f824d24aa33c40da3
summer-ng <94858729+summer-ng@users.noreply.github.com>
2021-11-27 03:30:46 +0800

    Update covariance_matrix.py

--- covariance_matrix.py
+++ covariance_matrix.py
@@ -33,11 +33,11 @@ def covariance_matrix(s_rt,beta_matrix,beta_expected_matrix,pca_cov,pca_array):
                         if x == 0 and y == 0:
                             single_cov = beta_matrix[x,y] 
                             whole_cov = whole_cov + single_cov
-                        elif x == 0 and y != 0:
-                            single_cov = pca_array[y-1] * beta_matrix[x,y]
+                        elif x != 0 and y == 0:
+                            single_cov = pca_array[x-1] * beta_matrix[x,y]
                             whole_cov = whole_cov + single_cov
                         else:
-                            single_cov = beta_expected_matrix[m,x]*beta_expected_matrix[m,y]*pca_cov[x-1,y-1] + pca_array[x-1]*pca_array[y-1]*beta_matrix[x,y] + pca_cov[x-1,y-1]*beta_matrix[x,y] 
+                            single_cov = beta_expected_matrix[m,x]*beta_expected_matrix[m,y]*pca_cov[x-1,y-1] + pca_array[x-1]*pca_array[y-1]*beta_matrix[x,y] + pca_cov[x-1,y-1]*beta_matrix.loc[x,y] 
                             whole_cov = whole_cov + single_cov
             whole_cov_matrix[m,n] = whole_cov
     return(whole_cov_matrix)
